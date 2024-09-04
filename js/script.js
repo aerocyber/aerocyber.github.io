aa5fd509e8c728ee9da4624fcc144d871fe63ee8
aerocyber <aerocyberdev@proton.me>
2024-09-04 19:01:31 +0530

    Redo site

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
-    const icn = document.getElementById('icon-toggler');
-    menu.classList.toggle("open");
-    icn.classList.toggle("bx-menu");
-    icn.classList.toggle("bx-x");
-    icon.classList.toggle("open");
-}
